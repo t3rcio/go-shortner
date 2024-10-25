1e7b46aedd4388688b610e81b0b35691114c58fa
Tercio <t3rcio@gmail.com>
2024-10-25 17:04:10 -0300

    Modulo Settings

---
+++ settings/go.mod
@@ -0,0 +1,1 @@
+module github.com/t3rcio/go-shortner/settings
---
+++ settings/settings.go
@@ -0,0 +1,9 @@
+/*
+* Settings
+ */
+
+package settings
+
+const TEMPLATES_ROOT = "templates"
+const DEBUG = true
+const INDEX_FILE = "index.html"
